ff4620afaa6dffcdd54531e2f3533e7033200cf9
tbdavid2019 <tbdavid2019@gmail.com>
2025-03-21 07:31:53 +0000

    忘記放上 requirements了

--- david.sh
+++ david.sh
@@ -1,8 +1,9 @@
 #建構  docker   ai-hedge-fund-API
 
 docker build -t ai-hedge-fund-api .
-docker run -p 6000:6000 ai-hedge-fund-api
-
+docker run --env-file .env -p 6000:6000 ai-hedge-fund-api
+docker tag ai-hedge-fund-api tbdavid2019/ai-hedge-fund-api:latest
+docker push tbdavid2019/ai-hedge-fund-api:latest
 
 
 python webui2.py
